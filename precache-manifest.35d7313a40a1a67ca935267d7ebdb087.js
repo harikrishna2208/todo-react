7f9bb9617b5a3f2996c0749a604b86816b1af99e
harikrishna2208 <harikrishna2278@gmail.com>
2020-09-08 15:55:09 +0530

    Updates

---
+++ precache-manifest.35d7313a40a1a67ca935267d7ebdb087.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "378ce05dc48c24004f34946379b75882",
+    "url": "/todo-react/index.html"
+  },
+  {
+    "revision": "0694c3f27ca4b11c197e",
+    "url": "/todo-react/static/css/main.22f41f40.chunk.css"
+  },
+  {
+    "revision": "e656a5ae134656000023",
+    "url": "/todo-react/static/js/2.1d81e151.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-react/static/js/2.1d81e151.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0694c3f27ca4b11c197e",
+    "url": "/todo-react/static/js/main.176b45ce.chunk.js"
+  },
+  {
+    "revision": "1614377f578425b5d648",
+    "url": "/todo-react/static/js/runtime-main.92cd31f8.js"
+  }
+]);
