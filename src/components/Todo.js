b5cc7d39b00cffa46e200b152fddb983fd0ddf7e
harikrishna2208 <harikrishna2278@gmail.com>
2020-09-08 15:54:08 +0530

    todo-react

---
+++ src/components/Todo.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+const Todo = ({ text, todo, setTodos, todos }) => {
+  const deletehandler = () => {
+    setTodos(todos.filter((el) => el.id !== todo.id));
+  };
+  const completeHdandler = () => {
+    setTodos(
+      todos.map((item) => {
+        if (item.id === todo.id) {
+          return {
+            ...item,
+            completed: !item.completed,
+          };
+        }
+        return item;
+      })
+    );
+  };
+  return (
+    <div className="todo">
+      <li className={`todo-item ${todo.completed ? "completed" : ""}`}>
+        {text}
+      </li>
+      <button onClick={completeHdandler} className="complete-btn">
+        <i className="fas fa-check"></i>
+      </button>
+      <button onClick={deletehandler} className="trash-btn">
+        <i className="fas fa-trash"></i>
+      </button>
+    </div>
+  );
+};
+
+export default Todo;
